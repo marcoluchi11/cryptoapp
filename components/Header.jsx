7013053fff1a67bf3546ca0de1edbe25b0736111
marcoluchi11 <marcoaluchi@hotmail.com>
2023-02-26 10:16:57 -0300

    add Link to back to Home

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,16 @@
 "use client";
 
+import Link from "next/link";
 import Login from "./Login";
 
 const Header = () => {
   return (
-    <header className="flex justify-around items-center">
-      <h1 className="text-4xl font-bold text-white  text-center my-10">
-        CryptoApp
-      </h1>
+    <header className="flex justify-between items-center mr-5 ml-5">
+      <Link href="/">
+        <h1 className="text-4xl font-bold text-white  text-center my-10 cursor-pointer">
+          CryptoApp
+        </h1>
+      </Link>
       <Login />
     </header>
   );
