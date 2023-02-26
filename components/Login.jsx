c596246b21e53d84657dde5053890e1376a7568c
marcoluchi11 <marcoaluchi@hotmail.com>
2023-02-26 10:17:20 -0300

    add /login route

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,9 +1,14 @@
 "use client";
+
+import Link from "next/link";
+
 const Login = () => {
   return (
     <ul className="flex gap-10 items-center">
       <li className="cursor-pointer hover:underline">Portfolio</li>
-      <li className="cursor-pointer hover:underline">Login</li>
+      <Link href="/login">
+        <li className="cursor-pointer hover:underline">Login</li>
+      </Link>
       <li className=" cursor-pointer font-bold bg-green-400 px-3 py-2 transition-colors hover:bg-white hover:text-green-400 rounded-md">
         Sign Up
       </li>
