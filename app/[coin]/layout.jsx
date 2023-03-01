eceeb2c01f4e37083f0f3eac791b782ccf56abcd
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-01 17:46:58 -0300

    add component to separate the page info

--- app/[coin]/layout.jsx
+++ app/[coin]/layout.jsx
@@ -1,10 +1,3 @@
-import Header from "@/components/Header";
-
 export default function CoinLayout({ children }) {
-  return (
-    <div>
-      <h1>Holis</h1>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- app/[coin]/page.jsx
+++ app/[coin]/page.jsx
@@ -1,22 +1,32 @@
+import Info from "@/components/Info";
+import MktCap from "@/components/MktCap";
 import Image from "next/image";
 
 const Coin = async ({ params }) => {
   const { coin } = params;
   const url = `https://api.coingecko.com/api/v3/coins/${coin.toLowerCase()}`;
   const data = await fetch(url);
-  console.log(url);
   const result = await data.json();
-  console.log(result);
   return (
-    <div className="flex flex-col items-center">
-      <h1>{result.id}</h1>
-      <h1>{result.symbol}</h1>
-      <h1>{result.name}</h1>
-      <h1>{result.description.en}</h1>
-      <Image src={result.image.large} alt="logo" height={100} width={100} />
-      <h1>{result.market_cap_rank}</h1>
-      <h1>{result.ath}</h1>
-      <h1>{result.market_data.current_price.usd.toLocaleString()}</h1>
+    <div className="flex flex-col lg:flex-row justify-evenly items-center">
+      <section className="w-full sm:w-2/3 md:1/2 mx-3">
+        <h1 className="">Rank #{result.market_cap_rank}</h1>
+        <div className="flex items-center ">
+          <h1 className="font-bold text-2xl">{result.name}</h1>
+          <h1 className="uppercase text-xl text-gray-500 mx-2">
+            {result.symbol}
+          </h1>
+          <Image src={result.image.large} alt="logo" height={35} width={35} />
+          <h3 className="text-red-500 ml-2 font-bold">
+            {result.market_data.price_change_percentage_24h.toFixed(2)}%
+          </h3>
+        </div>
+        <h1 className="font-bold text-3xl ">
+          ${result.market_data.current_price.usd.toLocaleString()}
+        </h1>
+        <MktCap data={result} />
+      </section>
+      <Info data={result} />
     </div>
   );
 };
