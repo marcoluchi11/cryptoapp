3992bce9d37f89451e131a732bb6e8c77c0b408a
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-04 09:40:57 -0300

    add better placement on smaller displays

--- components/Login.jsx
+++ components/Login.jsx
@@ -9,9 +9,11 @@ const Login = () => {
       <Link href="/login">
         <li className="cursor-pointer hover:underline">Login</li>
       </Link>
-      <li className=" cursor-pointer font-bold bg-green-400 px-3 py-2 transition-colors hover:bg-white hover:text-green-400 rounded-md">
-        Sign Up
-      </li>
+      <Link href="/signup">
+        <li className=" cursor-pointer font-bold bg-green-400 px-3 py-2 transition-colors hover:bg-white hover:text-green-400 rounded-md">
+          Sign Up
+        </li>
+      </Link>
     </ul>
   );
 };
