bc38bbafabe63b18d9d77df3aaa3c129a6bef585
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-04 09:40:15 -0300

    add form component for login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,37 +1,10 @@
 "use client";
+import Form from "@/components/Form";
 
-import { useState } from "react";
-
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const handleChange = (e) => setEmail(e.target.value);
-  const handleSubmit = (data, e) => {
-    // e.preventDefault();
-    console.log(data);
-  };
+export default function Login() {
   return (
-    <form
-      onSubmit={() => handleSubmit(email)}
-      className="flex flex-col items-center min-w-fit"
-    >
-      <label htmlFor="email" className="">
-        Email
-      </label>
-      <input
-        placeholder="Email"
-        onChange={handleChange}
-        type="email"
-        name="email"
-        id="email"
-        value={email}
-      />
-      <input
-        type="submit"
-        value="Log In"
-        className=" cursor-pointer px-4 py-2 m-5 font-bold bg-gray-500 rounded "
-      />
-    </form>
+    <section>
+      <Form />
+    </section>
   );
-};
-
-export default Login;
+}
