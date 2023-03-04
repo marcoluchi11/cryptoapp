fc5df71cbc0c8735f1c1fd0433dd6606cf63bec8
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-04 09:39:17 -0300

    add size to smaller displays

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,12 @@
 "use client";
-
 import Link from "next/link";
 import Login from "./Login";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center mr-5 ml-5">
+    <header className="flex justify-between items-center mr-5 ml-5 ">
       <Link href="/">
-        <h1 className="text-4xl font-bold text-white  text-center my-10 cursor-pointer">
+        <h1 className="text-xl md:text-4xl font-bold text-white  text-center my-10 cursor-pointer">
           CryptoApp
         </h1>
       </Link>
