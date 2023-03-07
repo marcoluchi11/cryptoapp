67d9aa12b424adf2257486276ca078aff607a0eb
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-07 17:26:23 -0300

    add onAuthStateChanged function and change the UI when logged in

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,8 +1,47 @@
 "use client";
 
+import auth from "@/config";
+import { CryptoContext } from "@/context/CryptoContext";
+import { onAuthStateChanged } from "firebase/auth";
 import Link from "next/link";
-
+import { useContext, useEffect } from "react";
+import { RxAvatar } from "react-icons/rx";
 const Login = () => {
+  const { user, setUser } = useContext(CryptoContext);
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+      console.log("User signed out successfully");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(currentUser);
+    });
+  }, [setUser]);
+  if (user)
+    return (
+      <ul className="flex gap-10 items-center">
+        <Link href="/portfolio">
+          <li className="cursor-pointer hover:underline">My Portfolio</li>
+        </Link>
+        <li className=" flex items-center text-ellipsis whitespace-nowrap overflow-hidden w-30">
+          <RxAvatar className="mx-3 cursor-text" size={30} />
+
+          <span className="hidden md:block">{user.email}</span>
+        </li>
+        <li
+          onClick={signOut}
+          className=" cursor-pointer font-bold bg-red-400 px-3 py-2 transition-colors hover:bg-white hover:text-red-400 rounded-md"
+        >
+          Sign Out
+        </li>
+      </ul>
+    );
+
   return (
     <ul className="flex gap-10 items-center">
       <li className="cursor-pointer hover:underline">Portfolio</li>
