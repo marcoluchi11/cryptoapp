76685dbafa6bcef0770de52e358a769a335376d5
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-07 17:24:16 -0300

    add user and pportfolio states

--- context/CryptoContext.js
+++ context/CryptoContext.js
@@ -7,9 +7,22 @@ export const CryptoProvider = ({ children }) => {
   const [error, setError] = useState({ state: false, message: "" });
   const [success, setSuccess] = useState({ state: false, message: "holu" });
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(false);
+  const [portfolio, setPortfolio] = useState([]);
   return (
     <CryptoContext.Provider
-      value={{ loading, error, success, setLoading, setError, setSuccess }}
+      value={{
+        loading,
+        error,
+        success,
+        setLoading,
+        setError,
+        portfolio,
+        setPortfolio,
+        setSuccess,
+        user,
+        setUser,
+      }}
     >
       {children}
     </CryptoContext.Provider>
