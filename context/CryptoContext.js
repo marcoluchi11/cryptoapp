c265e460056e0e005e958dc56a058e7ed5de6317
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-09 12:51:07 -0300

    add function that get coins for every user

--- context/CryptoContext.js
+++ context/CryptoContext.js
@@ -1,5 +1,8 @@
 "use client";
-const { createContext, useState, useContext } = require("react");
+import { database } from "@/config";
+import { doc, getDoc } from "firebase/firestore";
+
+import { createContext, useState, useContext } from "react";
 
 export const CryptoContext = createContext();
 
@@ -9,14 +12,32 @@ export const CryptoProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState(false);
   const [portfolio, setPortfolio] = useState([]);
+  const [coins, setCoins] = useState([]);
+  // const usersRef = collection(database, "users", "marq@gmail.com");
+
+  const getCoins = async () => {
+    console.log(user.email);
+    const docRef = doc(database, "users", user.email);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setPortfolio(docSnap.data().coins);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
   return (
     <CryptoContext.Provider
       value={{
         loading,
         error,
+        getCoins,
         success,
         setLoading,
         setError,
+        coins,
+        setCoins,
         portfolio,
         setPortfolio,
         setSuccess,
