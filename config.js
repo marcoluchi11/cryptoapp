2091ed0edf3235ec9ec6f97bbae0617a2faf8656
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-09 12:50:27 -0300

    add cloud firestore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,4 +12,5 @@ const firebaseConfig = {
 };
 const firebase_app = initializeApp(firebaseConfig);
 const auth = getAuth(firebase_app);
+export const database = getFirestore(firebase_app);
 export default auth;
